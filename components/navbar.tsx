3abd02d34db493d70a2e2c49be62f9b73a9ef1eb
Suman Mondal <mondalsumanrock20@gmail.com>
2025-03-02 01:14:56 +0530

    remove Algolia search props and component usage from Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,11 +31,11 @@ export const NAVLINKS = [
   },
 ];
 
-const algolia_props = {
-  appId: process.env.ALGOLIA_APP_ID!,
-  indexName: process.env.ALGOLIA_INDEX!,
-  apiKey: process.env.ALGOLIA_SEARCH_API_KEY!,
-};
+// const algolia_props = {
+//   appId: process.env.ALGOLIA_APP_ID!,
+//   indexName: process.env.ALGOLIA_INDEX!,
+//   apiKey: process.env.ALGOLIA_SEARCH_API_KEY!,
+// };
 
 export function Navbar() {
   return (
@@ -54,7 +54,7 @@ export function Navbar() {
         </div>
 
         <div className="flex items-center sm:justify-normal justify-between sm:gap-3 ml-1 sm:w-fit w-[90%]">
-          <AlgoliaSearch {...algolia_props} />
+          {/* <AlgoliaSearch {...algolia_props} /> */}
           <div className="flex items-center justify-between sm:gap-2">
             <div className="flex ml-4 sm:ml-0">
               <Link
