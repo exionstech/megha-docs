3cf59c3e18880de8b764574460771382a661e4f3
Suman Mondal <mondalsumanrock20@gmail.com>
2025-03-02 11:27:59 +0530

    refactor: update layout and branding for Megha Express, remove unused assets

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,20 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[85.5vh] min-h-[82vh] flex-col sm:items-center justify-center text-center px-2 sm:py-8 py-12">
       <Link
-        href="https://github.com/nisabmohd/Aria-Docs"
+        href="https://meghaexpress.in"
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4 sm:-mt-12"
       >
-        Follow along on GitHub{" "}
+        Ship With Us{" "}
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
-      <h1 className="text-[2.4rem] leading-10 sm:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center">
-        Effortlessly build stunning documentation sites with Next.js and server
-        components.
+      <h1 className="text-[2.4rem] leading-10 sm:leading-[4.5rem] font-bold mb-4 sm:text-6xl text-left sm:text-center md:w-[80%]">
+        API and Developement Guide for Megha Express
       </h1>
       <p className="mb-8 sm:text-lg max-w-[800px] text-muted-foreground text-left sm:text-center">
-        This feature-packed documentation template, built with Next.js, offers a
-        sleek and responsive design, perfect for all your project documentation
-        needs.
+        Megha Express is a logistics company that provides a simple and
+        efficient way to ship your products. Our API is designed to help you
+        integrate our services into your platform.
       </p>
       <div className="sm:flex sm:flex-row grid grid-cols-2 items-center sm;gap-5 gap-3 mb-8">
         <Link
@@ -32,20 +31,17 @@ export default function Home() {
           Get Stared
         </Link>
         <Link
-          href="/blog"
+          href="https://admin.meghaexpress.in"
+          target="_blank"
           className={buttonVariants({
             variant: "secondary",
             className: "px-6",
             size: "lg",
           })}
         >
-          Read Blog
+          Admin Panel
         </Link>
       </div>
-      <span className="sm:flex hidden flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-9 -mb-12 max-[800px]:mb-12 font-code sm:text-base text-sm font-medium border rounded-full p-2.5 px-5 bg-muted/55">
-        <TerminalSquareIcon className="w-5 h-5 sm:mr-1 mt-0.5" />
-        {"npx create-aria-doc <project-directory>"}
-      </span>
     </div>
   );
 }
